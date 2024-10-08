15b612eeecb1970f1638669a08199d626f91f6f4
OneSpace <118968197+OneSpaceerp@users.noreply.github.com>
2024-10-09 01:06:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-
 from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in nsd_installment/__init__.py
+from nsd_installment import __version__ as version
 
 setup(
     name='nsd_installment',
-    version='0.0.1',
+    version=version,
     description='Manage installment payments for sales orders.',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Nest Software Development',
+    author_email='info@nsd-eg.com',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=("frappe",)
+    install_requires=install_requires
 )
-    
